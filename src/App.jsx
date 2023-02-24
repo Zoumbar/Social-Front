c5aec4ab88399fabbd7f311ce7aa246249578234
Esteban <esteban.ouvre@gmail.com>
2023-02-24 15:50:36 +0100

    Mui and Theme provider setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,30 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings[mode]), [mode]);
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        {/* ROUTER */}
+        <ThemeProvider theme={theme}>
+          {/* Usable theme */}
+          <CssBaseline>
+            {/* Reset css provided by mui*/}
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/profile/:userId" element={<ProfilePage />} />
+            </Routes>
+          </CssBaseline>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
