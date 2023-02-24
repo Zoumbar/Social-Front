74e8836afe24756ea51d21969abd283a39c0f6c9
Esteban <esteban.ouvre@gmail.com>
2023-02-24 10:42:47 +0100

    File architecture and react-router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Home } from "@mui/icons-material";
+import { Login } from "@mui/icons-material";
 import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
