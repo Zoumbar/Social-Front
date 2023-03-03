36d1b26a49c27141e276eed2309f4d3542e37e03
Esteban <esteban.ouvre@gmail.com>
2023-03-03 14:59:53 +0100

    Feature: advert widget done

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -5,6 +5,7 @@ import Navbar from "../navbar/index";
 import UserWidget from "../../scenes/widgets/UserWidget";
 import MyPostWidget from "../../scenes/widgets/MyPostWidget";
 import PostsWidget from "../../scenes/widgets/PostsWidget";
+import AdvertWidget from "../../scenes/widgets/AdvertWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -32,7 +33,7 @@ const HomePage = () => {
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
-            {/* <AdvertWidget /> */}
+            <AdvertWidget />
             <Box m="2rem 0" />
             {/* <FriendListWidget userId={_id} /> */}
           </Box>
